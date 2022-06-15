6b4f6fd2149732bb6b8a95ddf11ed96b47bef807
Mauro Sarti <mauro.sarti@globant.com>
2022-06-15 00:31:25 -0300

    Design CodeLabs
    Tested in: Samsung S10+
    Signed off by: Mauro Sarti mauro.sarti@globant.com
    
    Design CodeLabs - Part 2
    
    Tested in: Samsung S10+
    Signed off by: Mauro Sarti mauro.sarti@globant.com

---
+++ DesignCodeLabs/app/src/main/java/com/example/composedesigncodelabs/ui/composable/Scaffold.kt
@@ -0,0 +1,52 @@
+package com.example.composedesigncodelabs.ui.composable
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.composedesigncodelabs.ui.theme.ComposeDesignCodeLabsTheme
+
+@Composable
+fun LayoutsCodeLabs() {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(text = "LayoutsCodeLab")
+                },
+                actions = {
+                    IconButton(onClick = { /* doSomething() */ }) {
+                        Icon(Icons.Filled.Favorite, contentDescription = null)
+                    }
+                }
+            )
+        }
+    ) { innerPadding ->
+        BodyContent(Modifier.padding(innerPadding))
+    }
+}
+
+@Composable
+private fun BodyContent(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.padding(8.dp)) {
+        Text(text = "Hi there!")
+        Text(text = "Thanks for going through the Layouts CodeLab")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScaffoldPreview() {
+    ComposeDesignCodeLabsTheme {
+        LayoutsCodeLabs()
+    }
+}
---
+++ DesignCodeLabs/app/src/main/java/com/example/composedesigncodelabs/ui/composable/ScrollingList.kt
@@ -0,0 +1,88 @@
+package com.example.composedesigncodelabs.ui.composable
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.IntrinsicSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.focus.focusModifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
+import com.example.composedesigncodelabs.ui.theme.ComposeDesignCodeLabsTheme
+import kotlinx.coroutines.launch
+
+@Composable
+fun ScrollingList() {
+    val listSize = 100
+    // We save the scrolling position with this state
+    val scrollState = rememberLazyListState()
+    // We save the coroutine scope where our animated scroll will be executed
+    val coroutineScope = rememberCoroutineScope()
+
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row {
+            Button(modifier = Modifier.weight(0.5f),
+                onClick = {
+                coroutineScope.launch {
+                    // 0 is the first item index
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text("Scroll to the top")
+            }
+
+            Button(modifier = Modifier.weight(0.5f),
+                onClick = {
+                coroutineScope.launch {
+                    // listSize - 1 is the last index of the list
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text("Scroll to the end")
+            }
+        }
+
+        LazyColumn(state = scrollState) {
+            items(listSize) {
+                ImageListItem(it)
+            }
+        }
+    }
+}
+
+@Composable
+private fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ScrollingListPreview() {
+    ComposeDesignCodeLabsTheme {
+        ScrollingList()
+    }
+}
